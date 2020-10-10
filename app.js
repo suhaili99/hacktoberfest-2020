93cac2a718cd2afe368f20104c659f1b9ec7548b
Mazc0de <dandroid36@gmail.com>
2020-10-10 16:01:42 +0700

    menambah saya

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const participants = [
-  {
-    name: "Wahyu Dwi Ramadhan",
-    github: "wdwiramadhan",
-    image: "images/wdwiramadhan.jpg",
-    address: "Semarang, Indonesia",
-  },
+const participants = [{
+        name: 'Wahyu Dwi Ramadhan',
+        github: 'wdwiramadhan',
+        image: 'images/wdwiramadhan.jpg',
+        address: 'Semarang, Indonesia'
+    },
+    {
+        name: 'Daffa Hanz',
+        github: 'mazc0de',
+        image: 'images/daffa.jpg',
+        address: 'Semarang, Indonesia'
+    }
 ];
