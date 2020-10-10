6d8c5f86f891ef22af7b32222c86a4496d47baa1
Mazc0de <dandroid36@gmail.com>
2020-10-10 16:25:20 +0700

    daffa

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const participants = [
-  {
-    name: "Wahyu Dwi Ramadhan",
-    github: "wdwiramadhan",
-    image: "images/wdwiramadhan.jpg",
-    address: "Semarang, Indonesia",
-  },
+const participants = [{
+        name: 'Wahyu Dwi Ramadhan',
+        github: 'wdwiramadhan',
+        image: 'images/wdwiramadhan.jpg',
+        address: 'Semarang, Indonesia'
+    },
+    {
+        name: 'Daffa Hanifisyafiq',
+        github: 'mazc0de',
+        image: 'images/mazc0de.jpg',
+        address: 'Pemalang, Indonesia'
+    }
 ];
