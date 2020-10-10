df3f84a701ba0daab606e3444c9626743ec37389
munchaminna <munc.haminna@gmail.com>
2020-10-10 18:12:50 +0700

    add image

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-const participants = [
-  {
-    name: "Wahyu Dwi Ramadhan",
-    github: "wdwiramadhan",
-    image: "images/wdwiramadhan.jpg",
-    address: "Jepara, Indonesia",
-  },
-  {
-    name: "Yahya Wildan",
-    github: "yahyawildan",
-    image: "images/wildan.png",
-    address: "Semarang, Indonesia",
-  },
-  {
-    name : "Suhaili Faruq",
-    github : "suhaili99",
-    image : "images/suhaili99",
-    address : "Pangkalan Bun, Indonesia"
-  },
-  {
-    name: 'Daffa Hanifisyafiq',
-    github: 'mazc0de',
-    image: 'images/mazc0de.jpg',
-    address: 'Pemalang, Indonesia'
-  },
- {
-    name: "Mohammad Lukman Hakim",
-    github: "loekem",
-    image: "images/loekem.jpg",
-    address: "Semarang, Indonesia",
-  },
-  {
-    name: "Dimas Seto Wicaksana",
-    github: "shironim",
-    image: "images/shironim.jpg",
-    address: "Semarang, Indonesia",
-  },
-  {
-    name: "Hilman Syaukani",
-    github: "hilmancrx",
-    image: "images/hilman.png",
-    address: "Tegal, Indonesia",
-  },
+const participants = [{
+        name: "Wahyu Dwi Ramadhan",
+        github: "wdwiramadhan",
+        image: "images/wdwiramadhan.jpg",
+        address: "Jepara, Indonesia",
+    },
+    {
+        name: "Yahya Wildan",
+        github: "yahyawildan",
+        image: "images/wildan.png",
+        address: "Semarang, Indonesia",
+    },
+    {
+        name: "Suhaili Faruq",
+        github: "suhaili99",
+        image: "images/suhaili99",
+        address: "Pangkalan Bun, Indonesia"
+    },
+    {
+        name: 'Daffa Hanifisyafiq',
+        github: 'mazc0de',
+        image: 'images/mazc0de.jpg',
+        address: 'Pemalang, Indonesia'
+    },
+    {
+        name: "Mohammad Lukman Hakim",
+        github: "loekem",
+        image: "images/loekem.jpg",
+        address: "Semarang, Indonesia",
+    },
+    {
+        name: "Dimas Seto Wicaksana",
+        github: "shironim",
+        image: "images/shironim.jpg",
+        address: "Semarang, Indonesia",
+    },
+    {
+        name: "Hilman Syaukani",
+        github: "hilmancrx",
+        image: "images/hilman.png",
+        address: "Tegal, Indonesia",
+    },
+    {
+        name: "Hilman Syaukani",
+        github: "hilmancrx",
+        image: "images/hilman.png",
+        address: "Tegal, Indonesia",
+    },
 ];
-
