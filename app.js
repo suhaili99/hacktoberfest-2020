aab3fe402787b3335710cb0b90f1b28b6f75b036
arif1713 <akram.pentol7@gmail.com>
2020-10-10 17:34:48 +0700

    add app.js

--- app.js
+++ app.js
@@ -5,4 +5,62 @@ const participants = [
     image: "images/wdwiramadhan.jpg",
     address: "Semarang, Indonesia",
   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  {
+    name: "arif rizaldi",
+    github: "arif1713",
+    image: "images/arif1713.jpg",
+    address: "Semarang, Indonesia",
+  }
 ];
