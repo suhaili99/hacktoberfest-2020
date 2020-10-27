f148c904157880cae106f81d8c84f35a0aa22a64
Mazc0de <dandroid36@gmail.com>
2020-10-27 15:16:26 +0700

    update app.js

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const participants = [
     image: "images/wdwiramadhan.jpg",
     address: "Semarang, Indonesia",
   },
+  {
+    name: "Agus Arifudin",
+    github: "Exterasz",
+    image: "images/agus.jpg",
+    address: "Pml, Indonesia",
+  },
 ];
