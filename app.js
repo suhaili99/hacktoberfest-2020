9f37bfa3c1bede960bdad1e7bcf8beb46b68e5cf
Mazc0de <dandroid36@gmail.com>
2020-10-27 15:26:52 +0700

    update v1 agusarifudin

--- app.js
+++ app.js
@@ -1,64 +1,67 @@
-const participants = [
-  {
-    name: "Wahyu Dwi Ramadhan",
-    github: "wdwiramadhan",
-    image: "images/wdwiramadhan.jpg",
-    address: "Jepara, Indonesia",
-  },
-   {
-    name: "agusarifudin",
-    github: "Exterasz",
-    image: "images/Exterasz.jpg",
-    address: "Pemalang, Indonesia",
-  },
-  {
-    name: "Yahya Wildan",
-    github: "yahyawildan",
-    image: "images/wildan.png",
-    address: "Semarang, Indonesia",
-  },
-  {
-    name: "arif rizaldi",
-    github: "arif1713",
-    image: "images/arif1713.jpg",
-    address: "Demak, Indonesia",
-  },
-  {
-    name : "Suhaili Faruq",
-    github : "suhaili99",
-    image : "images/suhaili99",
-    address : "Pangkalan Bun, Indonesia"
-  },
-  {
-    name: 'Daffa Hanifisyafiq',
-    github: 'mazc0de',
-    image: 'images/mazc0de.jpg',
-    address: 'Pemalang, Indonesia'
-  },
- {
-    name: "Mohammad Lukman Hakim",
-    github: "loekem",
-    image: "images/loekem.jpg",
-    address: "Semarang, Indonesia",
-  },
-  {
-    name: "Dimas Seto Wicaksana",
-    github: "shironim",
-    image: "images/shironim.jpg",
-    address: "Semarang, Indonesia",
-  },
-  {
-<<<<<<< HEAD
-    name: "Agus Arifudin",
-    github: "Exterasz",
-    image: "images/agus.jpg",
-    address: "Pml, Indonesia",
-=======
-    name: "Hilman Syaukani",
-    github: "hilmancrx",
-    image: "images/hilman.png",
-    address: "Tegal, Indonesia",
->>>>>>> dc0e0654f62c13987c9a27b8923b7f17e3957824
-  },
+const participants = [{
+        name: "Wahyu Dwi Ramadhan",
+        github: "wdwiramadhan",
+        image: "images/wdwiramadhan.jpg",
+        address: "Jepara, Indonesia",
+    },
+    {
+        name: "agusarifudin",
+        github: "Exterasz",
+        image: "images/Exterasz.jpg",
+        address: "Pemalang, Indonesia",
+    },
+    {
+        name: "Yahya Wildan",
+        github: "yahyawildan",
+        image: "images/wildan.png",
+        address: "Semarang, Indonesia",
+    },
+    {
+        name: "arif rizaldi",
+        github: "arif1713",
+        image: "images/arif1713.jpg",
+        address: "Demak, Indonesia",
+    },
+    {
+        name: "Suhaili Faruq",
+        github: "suhaili99",
+        image: "images/suhaili99",
+        address: "Pangkalan Bun, Indonesia"
+    },
+    {
+        name: 'Daffa Hanifisyafiq',
+        github: 'mazc0de',
+        image: 'images/mazc0de.jpg',
+        address: 'Pemalang, Indonesia'
+    },
+    {
+        name: "Mohammad Lukman Hakim",
+        github: "loekem",
+        image: "images/loekem.jpg",
+        address: "Semarang, Indonesia",
+    },
+    {
+        name: "Dimas Seto Wicaksana",
+        github: "shironim",
+        image: "images/shironim.jpg",
+        address: "Semarang, Indonesia",
+    },
+    { <<
+        <<
+        << < HEAD
+        name: "Agus Arifudin",
+        github: "Exterasz",
+        image: "images/agus.jpg",
+        address: "Pml, Indonesia",
+        ===
+        ===
+        =
+        name: "Hilman Syaukani",
+        github: "hilmancrx",
+        image: "images/hilman.png",
+        address: "Tegal, Indonesia",
+        >>>
+        >>>
+        > dc0e0654f62c13987c9a27b8923b7f17e3957824
+    },
 ];
-
